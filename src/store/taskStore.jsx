41e5a965c48fdc22f736cd543d4ecf55091d566d
Santiago parody <santiagocbk@gmail.com>
2024-03-12 16:31:25 -0300

    Se crean estados para contantes

--- src/store/taskStore.jsx
+++ src/store/taskStore.jsx
@@ -1,13 +1,28 @@
 import { create } from "zustand";
 import { persist } from "zustand/middleware";
+import { v4 } from "uuid";
 
 export const useTaskStore = create(
   persist(
     //
-    (set) => ({
+    (set, get) => ({
       tasks: [],
+      task: [],
+      statuses: ["backlog", "pending", "progress", "finish"],
+      statusIcons: {
+        backlog: "⛈️",
+        pending: "⭕",
+        progress: "🚧",
+        finish: "👍",
+      },
+      statusColor: {
+        backlog: "bg-red-300",
+        pending: "bg-orange-300",
+        progress: "bg-blue-300",
+        finish: "bg-green-300",
+      },
       addTask: (task) => {
-        const newTask = { ...task, status: "backlog", id: Date.now() };
+        const newTask = { ...task, status: "backlog", id: v4() };
         set((state) => ({
           tasks: [...state.tasks, newTask],
         }));
@@ -23,6 +38,24 @@ export const useTaskStore = create(
           tasks: state.tasks.map((task) =>
             task.id === id ? { ...task, status: status } : task
           ),
+        }));
+      },
+      // Pendiente
+      exportTasks: () => {
+        const tasksJSON = JSON.stringify(get().tasks);
+        const blob = new Blob([tasksJSON], { type: "application/json" });
+        const url = URL.createObjectURL(blob);
+        const a = document.createElement("a");
+        a.href = url;
+        a.download = "tasks.json";
+        document.body.appendChild(a);
+        a.click();
+        document.body.removeChild(a);
+        URL.revokeObjectURL(url);
+      },
+      findOneTask: (id) => {
+        set((state) => ({
+          task: state.tasks.find((task) => task.id === id),
         }));
       },
     }),
