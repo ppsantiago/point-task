941b39d2f13b2439b361e301734c4d15d8e86838
Santiago parody <santiagocbk@gmail.com>
2024-03-12 16:27:38 -0300

    Creacion de rutas

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,21 @@
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 //Providers
 import { NextUIProvider } from "@nextui-org/react";
+import Task from "./components/tasks/Task";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <NextUIProvider>
     <main className="dark text-foreground bg-[#141414] min-h-screen h-auto font-Montserrat ">
-      <App />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="/task/:id" element={<Task />} />
+        </Routes>
+      </BrowserRouter>
     </main>
   </NextUIProvider>
 );
